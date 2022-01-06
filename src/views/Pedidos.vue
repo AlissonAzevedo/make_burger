03af05bd7799c10bca2e38da39cb09db212de3e2
AlissonAzevedo <alissonazevedo13.@gmail.com>
2022-01-06 17:52:12 -0300

    Update Pedidos.vue

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="container">
-    <h1>Essa é a página de pedidos</h1>
+    <h1>Gerenciar Pedidos</h1>
+    <Dashboard/>
   </div>
 </template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+export default {
+  name: 'Pedidos',
+  components: {
+    Dashboard,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
